5463a83529879ec47e5b412dedaa8f8033014ad0
Olaniran Sulaimon <suzzwayne@gmail.com>
2020-07-15 02:10:55 +0100

    adds backdrop to slide navbar

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "70%";
+    document.getElementById("mySidenav").style.width = "70%" //opens side navbar by 70 percent
+    document.getElementById('backdrop').style.display = "block" //displays overlay
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0"
+    document.getElementById('backdrop').style.display = "none"
 }
 
 
 
-var slideIndex = [1, 1, 1, 1, 1, 1];
-var slideId = ["wedding-slides", "fashion-slides", "landscape-slides", "product-slides", "food-slides", "portrait-slides"]
+
+const slideIndex = [1, 1, 1, 1, 1, 1]; //
+const slideId = ["wedding-slides", "fashion-slides", "landscape-slides", "product-slides", "food-slides", "portrait-slides"]
 showDivs(1, 0);
 showDivs(1, 1);
 showDivs(1, 2);
@@ -22,20 +25,20 @@ function plusDivs(n, no) {
 }
 
 function showDivs(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
   if (n > x.length) {slideIndex[no] = 1}
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  x[slideIndex[no]-1].style.display = "block";
 }
 
 
 
 
-//Animate Smooth Scrolling
+//Animate Smooth Scrolling with jquery
 
 $('#home-link, #home-link-desktop').on('click', function(){
     const home = $('#home').position().top
